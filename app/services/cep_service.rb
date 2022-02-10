1d151d0b5c210d00523b99cf5d01d87a67a8679a
Hendrew <hendrew.developer@gmail.com>
2022-02-10 18:12:31 -0300

    chore: improvements perform method

--- app/services/cep_service.rb
+++ app/services/cep_service.rb
@@ -1,15 +1,11 @@
 # frozen_string_literal: true
 
-class CepService < ApplicationService
-  def initialize(cep_value)
-    @cep_value = cep_value
-  end
-
-  def perform
-    response = Faraday.get("https://viacep.com.br/ws/#{@cep_value}/json/")
+class CepService
+  def perform(cep_value)
+    response = Faraday.get("https://viacep.com.br/ws/#{cep_value}/json/")
 
     return {} unless response.status == 200
 
-    json = JSON.parse(response.body, symbolize_names: true)
+    JSON.parse(response.body, symbolize_names: true)
   end
 end
