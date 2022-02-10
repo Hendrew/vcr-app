50e0f66e93be73751d9561542220c28ca80065ff
Hendrew <hendrew.developer@gmail.com>
2022-02-10 18:00:50 -0300

    feat: add ceps controller

---
+++ app/controllers/api/v1/ceps_controller.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class CepsController < ApplicationController
+      def index
+        cep_value = params[:cep_value]
+        data = {}
+
+        data = CepService.perform(cep_value) if cep_value.present?
+
+        render json: { data: }, status: :ok
+      end
+    end
+  end
+end
