7f5d246321c50ed19cde005211c0ec7e7efacf93
Hendrew <hendrew.developer@gmail.com>
2022-02-10 18:12:05 -0300

    chore: improvements call to cep service

--- app/controllers/api/v1/ceps_controller.rb
+++ app/controllers/api/v1/ceps_controller.rb
@@ -7,7 +7,7 @@ module Api
         cep_value = params[:cep_value]
         data = {}
 
-        data = CepService.perform(cep_value) if cep_value.present?
+        data = CepService.new.perform(cep_value) if cep_value.present?
 
         render json: { data: }, status: :ok
       end
